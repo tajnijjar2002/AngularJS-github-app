c0aeefd481d091bcfbc4116794570a2a30fa5b93
tajveer nijjar <taj.nijjar2002@gmail.com>
2017-03-10 23:45:58 +1300

    Registered a module and added a controller

---
+++ AngularJS scott allen again/App/script.js
@@ -0,0 +1,17 @@
+﻿(function()
+{
+                
+
+    var MainController = function ($scope)
+    {
+        $scope.message = "hello";
+    };
+
+
+    angular
+        .module("githubViewer")
+        .controller("MainController", ['$scope', MainController]);
+
+
+    //app.controller("MainController", MainController());
+})();
