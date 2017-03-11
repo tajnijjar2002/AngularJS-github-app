0dc98963521250143d658fedb629e0af171a8999
tajveer nijjar <taj.nijjar2002@gmail.com>
2017-03-11 18:23:49 +1300

    Implemented SERVICES

--- AngularJS scott allen again/App/script.js
+++ AngularJS scott allen again/App/script.js
@@ -1,21 +1,72 @@
-﻿(function()
+﻿(function ()
 {
 
-    var MainController = function ($scope, $http)
+
+    var MainController = function (
+        $scope, github, $interval,
+        $location, $anchorScroll)
     {
-        $http.get("https://api.github.com/users/robconery")
-            .then(function(response)
+        var onUserComplete = function (data)
+        {
+            $scope.user = data;
+            github.getRepos($scope.user)
+                 .then(onRepos, onError);
+        };
+
+        var onRepos = function(data)
+        {
+            $scope.repos = data;
+
+            $location.hash("userDetails");
+            $anchorScroll(); 
+        }
+
+        var onError = function (reason)
+        {
+            $scope.error = "Some error occured!";
+        };
+
+        var decrementCountdown = function()
+        {
+            $scope.countdown--;
+            if ($scope.countdown < 1)
             {
-                $scope.user = response.data;
-            },
-            function(reason)
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+        var startCountdown = function()
+        {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function (username)
+        {
+            github.getUser(username)
+                .then(onUserComplete, onError);
+
+
+            if (countdownInterval)
             {
-                $scope.error = "Some error occured!";
-            });
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+        }
+
+        $scope.repoSortOrder = '-stargazers_count';
+        $scope.message = "Hello GithubViewer";
+        $scope.username = "Angular";
+        $scope.countdown = 5;
+
+        startCountdown();
     };
 
 
     //Registering a controller with the module.
-    app.controller("MainController", ['$scope', '$http', MainController]);
+    app.controller("MainController",
+    [
+        '$scope', 'github', '$interval',
+        '$location', '$anchorScroll', MainController]);
 
 })();
